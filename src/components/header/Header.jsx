9d8a4ce546e73723fad56c00648cd7fedf65beef
alexjbustillos <alexjbustillos@gmail.com>
2022-05-17 02:28:29 -0400

    checking if updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import Logo from '../../assets/logo.png'
+// import Logo from '../../assets/logo.png'
 import HeaderSocials  from './HeaderSocials'
 const Header = () => {
     return (
         <header>
            <div className="container header_container">
-                <h5>Hello I'm</h5>   
-                <h1>Alex Bustillos</h1>
-                <h5 className="text-light">Fullstack Developer</h5>
-                <CTA/>   
+                <div className="tranparent__background">
+                    <h5>Hello I'm</h5>   
+                    <h1>Alex Bustillos</h1>
+                    <h5 className="text-light">Fullstack Developer</h5>
+                    <CTA/>   
+                </div>
                 <HeaderSocials/>
                 
-                <div className="me">
+                {/* <div className="me">
                     <img src={Logo} alt="logo" />
-                </div>
+                </div> */}
                 <a href="#contact" className="scroll__down">Scroll Down</a>
             </div> 
         </header>
