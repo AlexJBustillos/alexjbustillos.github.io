72323631a19b586d4024692a4513f78af55944c0
alexjbustillos <alexjbustillos@gmail.com>
2022-05-17 03:08:32 -0400

    footer permalinks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
     return (
         <footer>
             <a href="#" className='footer__logo'>Alex J Bustillos</a>
-            <ul className='permalinks'>
+            {/* <ul className='permalinks'>
                 <li><a href="#">Home</a></li>
-                <li><a href="about">About</a></li>
-                <li><a href="experience">Experience</a></li>
-                <li><a href="portfolio">Projects</a></li>
-                <li><a href="contact">Contact</a></li>
-            </ul>
+                <li><a href="#/about">About</a></li>
+                <li><a href="#/experience">Experience</a></li>
+                <li><a href="#/portfolio">Projects</a></li>
+                <li><a href="#/contact">Contact</a></li>
+            </ul> */}
 
             {/* <div className="footer__socials">
                 <a href="https://instagram.com"><FiInstagram/></a>
